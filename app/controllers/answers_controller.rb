74ee62e060df6551ee36c291bb0aae2ed0efec18
sonu-buddh <veeraryan92@gmail.com>
2021-09-06 17:02:00 +0530

    add answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -25,7 +25,7 @@ class AnswersController < ApplicationController
 
     respond_to do |format|
       if @answer.save
-        format.html { redirect_to @answer, notice: "Answer was successfully created." }
+        format.html { redirect_to root_path, notice: "Answer was successfully created." }
         format.json { render :show, status: :created, location: @answer }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @answer = Answer.new
     @questions = Question.all
   end
   
